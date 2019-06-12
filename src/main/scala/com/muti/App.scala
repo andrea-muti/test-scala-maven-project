c6dc8f62cca4ece0a8c6c0bf5d6cf2bd549fdc41
Andrea Muti <andrea.muti@eng.it>
2019-06-12 14:37:34 +0200

    added line

--- src/main/scala/com/muti/App.scala
+++ src/main/scala/com/muti/App.scala
@@ -28,5 +28,7 @@ object App {
     println( "Hello World!" )
     println("concat arguments = " + foo(args))
     println("This line was added into the 'develop' branch")
+
+    println("Again, this is a second new line, added to develop after creating the new-feature branch")
   }
 }
